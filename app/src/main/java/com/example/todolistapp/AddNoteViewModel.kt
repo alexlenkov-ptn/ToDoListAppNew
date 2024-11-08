5706c71558a14026dd5ceaab7c47b6bddd5265ef
alexlenkov-ptn <alex.lenkov123@outlook.com>
2024-11-08 10:11:25 +0300

    add

--- app/src/main/java/com/example/todolistapp/AddNoteViewModel.kt
+++ app/src/main/java/com/example/todolistapp/AddNoteViewModel.kt
@@ -2,12 +2,16 @@ package com.example.todolistapp
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.functions.Action
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class AddNoteViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -16,15 +20,25 @@ class AddNoteViewModel(application: Application) : AndroidViewModel(application)
 
     var shouldCloseScreen: MutableLiveData<Boolean> = MutableLiveData(false)
 
+    private val compositeDisposable : CompositeDisposable = CompositeDisposable()
+
+    private lateinit var disposable : Disposable
+
     fun saveNote(note: Note) {
 
-        notesDao.addNote(note)
+        disposable = notesDao.addNote(note)
+            .delay(5, TimeUnit.SECONDS)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
+                Log.d("AddNoteViewModel", "subscribe")
                 shouldCloseScreen.postValue(true)
             }
-
+        compositeDisposable.add(disposable)
     }
 
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
 }
